6c5641f1540382078998ee29d51522cfd9267e42
DinethGR <dinethgimhana20@gmail.com>
2023-10-12 21:16:09 +0530

    Test

--- Guess-Game.py
+++ Guess-Game.py
@@ -1,27 +1,23 @@
 #Welcome to the math guessing game made by Dineth Gimhana : Github(DinethGR)
+#---------------------------
+#The Math Guessing Game 1.0v
 #===========================================================================
 
-#print welcome massage to players
 print("Welcome To The Math Guessing Game")
 
-#Enter the input for starting guessing game
 G = input("Enter The Number:")
 
-#Input assign to string 
 Guess = int(G)
 
-#Condition-1
 if Guess == 5:
     print("Congratulation...!")
     print("You Win..!")
 
-#Condition-2
 else:
     print("Oops...")
     print("You Lose..!")
     print("Try Again..")
 
-#Thanks for plyaing this game
 print("<<<< GAME OVER >>>>")
 
 #=========================================================================
